420c091e271eccce531cc42581ff58dceb8d9b05
Egor Gavrilov <egoregorgavrilov@gmail.com>
2023-10-29 02:06:07 +0200

    make project(no-adaptive)

--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,6 @@
 document.querySelector('.burger').addEventListener('click', function () {
 	this.classList.toggle('active');
 	document.querySelector('.nav').classList.toggle('open');
+	document.querySelector('.header__btn').classList.toggle('open');
 })
+
