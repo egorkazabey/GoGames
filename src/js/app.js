fd38a90b92e6a94d145ac9cb92b7111f798fd687
Egor Gavrilov <egoregorgavrilov@gmail.com>
2023-10-30 17:46:14 +0100

    last commit

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,32 @@ document.querySelector('.burger').addEventListener('click', function () {
 	document.querySelector('.header__btn').classList.toggle('open');
 })
 
+const swiper = new Swiper('.swiper', {
+
+    //! Основные настройки 
+    direction: 'horizontal', // 'vertical', 'horizontal'
+    loop: true, // true - круговой слайдер, false - слайдер с конечными положениями
+    speed: 500, // скорость переключения слайдов
+    effect: 'coverflow', // cards, coverflow, flip, fade, cube
+    // initialSlide: 2, // Начинаем со 2 слайдера
+    // freeMode: true, // можно перетаскивать как ленту
+    slidesPerView: 1, // кол-во активных слайдов
+    // centeredSlides: true, // центрирование слайдов
+    
+    //! Пагинация (точки)
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true, // true - Пагинация становится кликабельной
+    },
+
+    //! Кнопки вперед и назад 
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    //! Автоматическое перелистывание
+    // autoplay: {
+    //     delay: 1000, //Задержка перед перелистыванием 1с = 1000мл/с
+    // },
+});
